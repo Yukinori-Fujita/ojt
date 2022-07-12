274f38619c7c8ecee2f8745ac88a12cd42f0fec2
藤田 幸法 <straight.0429@gmail.com>
2022-07-12 21:57:39 +0900

    全部に<?php echo get_template_directory_uri();?>をかけて表示させました。

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css">
-    <link rel="stylesheet" href="./reset.css">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./assets/css/yukinori.css">
-    <link rel="stylesheet" href="./assets/css/gushiken.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/yukinori.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/gushiken.css">
     <title>ojt</title>
 </head>
 <body>
@@ -31,15 +31,15 @@
 
     <section id="kv">
         <picture id="logo">
-            <img src="./img/logo.png" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/img/logo.png" alt="">
         </picture>
         <picture id="main-visual">
             <source media="(max-width: 767px)" srcset="img/visual_1_sp.png">
-            <img src="./img/visual_1_pc.png" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/img/visual_1_pc.png" alt="">
         </picture>
         <picture id="visual-text">
             <source media="(max-width: 767px)" srcset="img/visual_text_sp.png">
-            <img src="./img/visual_text_pc.png" alt="">
+            <img src="<?php echo get_template_directory_uri();?>/img/visual_text_pc.png" alt="">
         </picture>
         <p>進化し続ける「街」<br class="br-sp">アメリカンビレッジマガジン</p>
     </section>
@@ -51,7 +51,7 @@
             <div class="box-articles1">
 
                 <div class="ariticles">
-                    <img src="./img/post_img_1.png" alt="カフェ"></a>
+                    <img src="<?php echo get_template_directory_uri();?>/img/post_img_1.png" alt="カフェ"></a>
                     <time datetime="">2018/5/20</time>
                     <p>おしゃれカフェがありますよ</p>
                     <div class="underline">
@@ -60,7 +60,7 @@
                     </div>
                 </div>
                 <div class="ariticles">
-                    <img src="./img/post_img_2.png" alt="観覧車"></a>
+                    <img src="<?php echo get_template_directory_uri();?>/img/post_img_2.png" alt="観覧車"></a>
                     <time datetime="">2018/5/19</time>
                     <p>あのネオンはいつ交換するのか！？観覧者の謎に迫る！</p>
                     <div class="underline">
@@ -69,7 +69,7 @@
                     </div>
                 </div>
                 <div class="ariticles">
-                    <img src="./img/post_img_3.png" alt="社内"></a>
+                    <img src="<?php echo get_template_directory_uri();?>/img/post_img_3.png" alt="社内"></a>
                     <time datetime="">2018/5/18</time>
                     <p>ラソナの社内はこんなのよ</p>
                     <div class="underline">
@@ -80,7 +80,7 @@
 
 
                 <div class="ariticles">
-                    <img src="./img/post_img_4.png" alt="アラハビーチ"></a>
+                    <img src="<?php echo get_template_directory_uri();?>/img/post_img_4.png" alt="アラハビーチ"></a>
                     <time datetime="">2018/5/17</time>
                     <p>お隣のアラハはハワイ？</p>
                     <div class="underline">
@@ -89,7 +89,7 @@
                     </div>
                 </div>
                 <div class="ariticles">
-                    <img src="./img/post_img_5.png" alt="テント"></a>
+                    <img src="<?php echo get_template_directory_uri();?>/img/post_img_5.png" alt="テント"></a>
                     <time datetime="">2018/5/16</time>
                     <p>なぜテント？ラソナの人にきいてみた</p>
                     <div class="underline">
@@ -98,7 +98,7 @@
                     </div>
                 </div>
                 <div class="ariticles">
-                    <img src="./img/post_img_6.png" alt="ベイエリア"></a>
+                    <img src="<?php echo get_template_directory_uri();?>/img/post_img_6.png" alt="ベイエリア"></a>
                     <time datetime="">2018/5/15</time>
                     <p>ベイエリアおしゃれすぎる問題</p>
                     <div class="underline">
@@ -124,7 +124,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    <script src="./script.js"></script>
+    <script src="<?php echo get_template_directory_uri();?>/script.js"></script>
 </body>
 
 </html>
