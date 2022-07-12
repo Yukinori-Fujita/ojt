7a0dc3a5cf37173d2db9bc02bb6c5137d3e1002a
藤田 幸法 <straight.0429@gmail.com>
2022-07-12 15:29:26 +0900

    slick入れてみました！

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ $(function () {
         $(this).toggleClass("on");
         $("ul").toggleClass("hide");
     })
+
+    $("#main-visual").slick({
+        "autoplay": true,
+        "autoplaySpeed": 3000,
+        "fade": true,
+        "variableWidth": true,
+        "adaptiveHeight	": true,
+        "arrows": false,
+    });
 })
