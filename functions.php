de9b584b6bcb2cc3e2114123d59431ad17f02efb
藤田 幸法 <straight.0429@gmail.com>
2022-07-16 01:18:43 +0900

    微調整

--- functions.php
+++ functions.php
@@ -1,10 +1,4 @@
 <?php
     add_theme_support("post-thumbnails");
     add_theme_support( 'menus' );
-
-// $custom_header_defaults = array(
-// 	"default-image" => get_bloginfo("template_url")."/images/hero-default.jpg",
-// 	"width" => 1034,
-// 	"height" => 448,
-// 	);
 ?>
